6584c991fa285b188642dc13825000ef2600862c
Jerome Cheng <jcheng31@gmail.com>
2013-08-22 20:20:33 +0800

    Prettify template.

--- js/templates.js
+++ js/templates.js
@@ -1,7 +1,12 @@
 var Templates = {};
 
-Templates.ItemListingTemplate = _.template('<div class="item media">' +
-	'<img class="pull-left" src="<%= imageUrl %>" /><div class="media-body">' +
-	'<h3 class="media-heading"><%= name %></h3><dv class="item-location"><strong>Location:</strong> ' +
-	'<%= location %></div><div class="item-price"><%= price %></div><div class="item-buyers"><%= buyers.length %>' +
-	'</div></div></div>');
+Templates.ItemListingTemplate = _.template('\
+	<div class="item media">\
+		<img class="pull-left" src="<%= imageUrl %>" />\
+		<div class="media-body">\
+			<h3 class="media-heading"><%= name %></h3>\
+			<dv class="item-location"><strong>Location:</strong> <%= location %></div>\
+			<div class="item-price"><%= price %></div>\
+			<div class="item-buyers"><%= buyers.length %></div>\
+		</div>\
+	</div>');
