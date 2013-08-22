19224287e8411bcaaaca460053fde81f2597890a
Jerome Cheng <jcheng31@gmail.com>
2013-08-22 22:56:51 +0800

    Fix incorrect spelling of initialize, simplify rendering.

--- js/views.js
+++ js/views.js
@@ -1,35 +1,40 @@
 var Views = {};
 Views.ListingView = Backbone.View.extend({
-	initialise: function() {
-		this.listenTo(this.collection, 'add', this.render);
+	initialize: function() {
+		this.listenTo(this.collection, 'add', this.collectionAdded);
 		this.listenTo(this.collection, 'change', this.render);
 		this.listenTo(this.collection, 'remove', this.render);
+		
 		this.childViews = [];
+
+		var that = this;
+		this.collection.each(function(item) {
+			that.collectionAdded(item);
+		});
 	},
 
 	render: function() {
-		this.childViews = [];
-
 		var fragment = document.createDocumentFragment();
 
-		var currentChildView;
-		var that = this;
-		this.collection.each(function(currentModel) {
-			currentChildView = new Views.ItemView({
-				model: currentModel
-			});
-			that.childViews.push(currentChildView);
-			fragment.appendChild(currentChildView.render().el);
+		_(this.childViews).each(function(currentView) {
+			fragment.appendChild(currentView.render().el);
 		});
 
 		this.$el.html(fragment);
 		return this;
+	},
+
+	collectionAdded: function(item) {
+		this.childViews.push(new Views.ItemView({
+			model: item
+		}));
+		this.render();
 	}
 });
 
 Views.ItemView = Backbone.View.extend({
 	template: Templates.ItemListingTemplate,
-	initialise: function() {
+	initialize: function() {
 		this.listenTo(this.model, "change", this.render);
 		this.id = "item-" + this.model.attributes.id;
 	},
