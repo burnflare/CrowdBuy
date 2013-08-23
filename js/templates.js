e3de102b5bb15ae96ccbd3f68fdb9b479f4da4e1
Jerome Cheng <jcheng31@gmail.com>
2013-08-23 15:26:48 +0800

    Add buyer suffix to template.

--- js/templates.js
+++ js/templates.js
@@ -8,6 +8,6 @@ Templates.ItemListingTemplate = _.template('\
 			<div class="item-expiry"><span class="glyphicon glyphicon-calendar"></span><%= dateEnd.toDateString() %></div>\
 			<div class="item-location"><span class="glyphicon glyphicon-globe"></span> <%= location %></div>\
 			<div class="item-price"><span class="glyphicon glyphicon-usd"></span><%= price %></div>\
-			<div class="item-buyers"><span class="glyphicon glyphicon-user"></span><%= buyers.length %></div>\
+			<div class="item-buyers"><span class="glyphicon glyphicon-user"></span><%= buyers.length %> <% if(buyers.length == 1) { %> buyer <% } else { %> buyers <% } %></div>\
 		</div>\
 	</div>');
