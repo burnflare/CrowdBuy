49078f5d1c8bfd2510ba39414279dd1756dff22e
Jerome Cheng <jcheng31@gmail.com>
2013-08-23 15:04:02 +0800

    Add icons to item template.

--- js/templates.js
+++ js/templates.js
@@ -5,8 +5,8 @@ Templates.ItemListingTemplate = _.template('\
 		<img class="pull-left" src="<%= imageUrl %>" />\
 		<div class="media-body">\
 			<h3 class="media-heading"><%= name %></h3>\
-			<div class="item-location"><strong>Location:</strong> <%= location %></div>\
-			<div class="item-price"><%= price %></div>\
+			<div class="item-location"><span class="glyphicon glyphicon-globe"></span> <%= location %></div>\
+			<div class="item-price"><span class="glyphicon glyphicon-usd"></span><%= price %></div>\
 			<div class="item-buyers"><%= buyers.length %></div>\
 		</div>\
 	</div>');
