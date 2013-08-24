78031932274fe7d201aa2bfaf6efa44477d20f23
Jerome Cheng <jcheng31@gmail.com>
2013-08-24 22:59:55 +0800

    Use index.html for requirejs, finish defining initial setup.

--- js/app.js
+++ js/app.js
@@ -1,12 +1,21 @@
 requirejs.config({
 	"baseUrl": "js/lib",
+	shim: {
+		'facebook' : {
+			export: 'FB'
+		}
+	},
 	"paths": {
 		"jquery": "//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js",
 		"text": "text.js",
 		"underscore": "underscore.min.js",
 		"backbone": "backbone.min.js",
-		"app": "../app"
+		"facebook": "//connect.facebook.net/en_US/all"
 	}
 });
 
-requirejs(["jquery", "text", "underscore", "backbone", "app/main"],);
+requirejs(["jquery", "text", "underscore", "backbone", "fb"], function($, _, Backbone, FB, App) {
+	new App({
+		el: $('#application');
+	})
+});
