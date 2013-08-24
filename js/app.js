a62387f027b802be2668797b8645b6f06c7c7ad0
Jerome Cheng <jcheng31@gmail.com>
2013-08-24 17:21:11 +0800

    Broken requirejs implementation.

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ requirejs.config({
 	"baseUrl": "js/lib",
 	"paths": {
 		"app": "../app",
-		"jquery": "//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min"
+		"jquery": "//ajax.googleapis.com/ajax/libs/jquery/jquery-1.10.2/jquery.min"
 	}
 });
 
