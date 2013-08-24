0302d113fbba3c47756a33758cc95100517be6b1
Jerome Cheng <jcheng31@gmail.com>
2013-08-25 01:27:55 +0800

    Use closure to fix context issue.

--- js/app/views/main.js
+++ js/app/views/main.js
@@ -1,84 +1,87 @@
 define(['jquery', 'underscore', 'backbone', 'text!./app/views/templates/main.html', 'models', 'facebook'], function($, _, Backbone, mainTemplate, Models) {
-	var Views = {};
-	Views.Main = Backbone.View.extend({
-		initialize: function() {
-			this.$el.html(_.template(mainTemplate, {}));
+		var Views = {};
+		Views.Main = Backbone.View.extend({
+				initialize: function() {
+					this.$el.html(_.template(mainTemplate, {}));
 
-			FB.init({
-			  appId: '509825915758193',
-			  channelUrl: '//http://crowdbuy.sapuan.org/channel.html',
-			});     
-			
-			$('#loginbutton,#feedbutton').removeAttr('disabled');
-
-			FB.getLoginStatus(this._authenticationCallback);
-		},
-
-		_authenticationCallback: function(response) {
-				if (response.status === 'connected') {
-					// Handle authentication here.
-
-					FB.api('/me', function(response) {
-						$('#welcome').html('Welcome, ' + response.name + '!');
+					FB.init({
+						appId: '509825915758193',
+						channelUrl: '//http://crowdbuy.sapuan.org/channel.html',
 					});
-			        this._setUpCollections();
-				} else {
-					alert("Whoa, something went wrong! Try refreshing this page.");
+
+					$('#loginbutton,#feedbutton').removeAttr('disabled');
+
+					FB.getLoginStatus((function(that) {
+						return function(response) {
+							if (response.status === 'connected') {
+								// Handle authentication here.
+
+								FB.api('/me', function(response) {
+									$('#welcome').html('Welcome, ' + response.name + '!');
+								});
+								that._setUpCollections();
+							} else {
+								alert("Whoa, something went wrong! Try refreshing this page.");
+							}
+						}
+					})(this));
 				}
-		},
+			},
 
-		_setUpCollections: function() {
-			yourCollection = new Models.Wants();
-			friendCollection = new Models.Wants();
-			featuredCollection = new Models.Wants();
-			publicCollection = new Models.Wants();
+			_authenticationCallback: function(response) {},
 
-			yourView = new Views.ListingView({
-				collection: yourCollection,
-				el: document.getElementById('you-section'),
-				id: "you"
-			});
+			_setUpCollections: function() {
+				yourCollection = new Models.Wants();
+				friendCollection = new Models.Wants();
+				featuredCollection = new Models.Wants();
+				publicCollection = new Models.Wants();
 
-			friendView = new Views.ListingView({
-				collection: friendCollection,
-				el: document.getElementById('friend-section'),
-				id: "friend"
-			});
+				yourView = new Views.ListingView({
+					collection: yourCollection,
+					el: document.getElementById('you-section'),
+					id: "you"
+				});
 
-			featuredView = new Views.ListingView({
-				collection: featuredCollection,
-				el: document.getElementById('featured-section'),
-				id: "featured"
-			});
+				friendView = new Views.ListingView({
+					collection: friendCollection,
+					el: document.getElementById('friend-section'),
+					id: "friend"
+				});
 
-			publicView = new Views.ListingView({
-				collection: publicCollection,
-				el: document.getElementById('public-section'),
-				id: "public"
-			});
+				featuredView = new Views.ListingView({
+					collection: featuredCollection,
+					el: document.getElementById('featured-section'),
+					id: "featured"
+				});
 
-			yourCollection.fetch({
-				data: $.param({
-					type: 'you'
-				})
-			});
-			friendCollection.fetch({
-				data: $.param({
-					type: 'friend'
-				})
-			});
-			featuredCollection.fetch({
-				data: $.param({
-					type: 'featured'
-				})
-			});
-			publicCollection.fetch({
-				data: $.param({
-					type: 'public'
-				})
-			});
-		}
-	});
+				publicView = new Views.ListingView({
+					collection: publicCollection,
+					el: document.getElementById('public-section'),
+					id: "public"
+				});
+
+				yourCollection.fetch({
+					data: $.param({
+						type: 'you'
+					})
+				});
+				friendCollection.fetch({
+					data: $.param({
+						type: 'friend'
+					})
+				});
+				featuredCollection.fetch({
+					data: $.param({
+						type: 'featured'
+					})
+				});
+				publicCollection.fetch({
+					data: $.param({
+						type: 'public'
+					})
+				});
+			}
+		});
 
 	return Views;
 });
