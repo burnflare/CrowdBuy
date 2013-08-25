1fede693ea8a270906108669a9e0629cd09818e2
Jerome Cheng <jcheng31@gmail.com>
2013-08-25 19:03:56 +0800

    Fetch on init.

--- js/app/models/searchResults.js
+++ js/app/models/searchResults.js
@@ -1,6 +1,9 @@
 define(['jquery', 'underscore', 'backbone', 'model_search_item'], function($, _, Backbone, Models) {
 	Models.SearchResults = Backbone.Collection.extend({
 		model: Models.SearchItem,
+		initialize: function() {
+			this.fetch();
+		}
 		parse: function(response, options) {
 			return response.result.results;
 		}
