a2f3c8ebee44de3a84009b3d505d5d5fee44fa78
Joel Low <joel@joelsplace.sg>
2013-08-25 20:13:02 +0800

    We don't need the auth token to be given to us explicitly (again)

--- js/app/utils.js
+++ js/app/utils.js
@@ -36,10 +36,7 @@ define(["jquery", "underscore", "backbone"], function($, _, Backbone) {
 
 	Utils.logIn = function(response) {
 		$.ajax({
-			url: '/service/me/login?' +
-				'auth[accessToken]=' + Utils.urlencode(response.authResponse.accessToken) +
-				'&auth[expiresIn]=' + parseInt(response.authResponse.expiresIn) +
-				'&auth[userID]=' + Utils.urlencode(response.authResponse.userID),
+			url: '/service/me/login',
 			async: false,
 			dataType: 'json'
 		});
