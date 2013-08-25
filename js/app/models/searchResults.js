1f1c7baa0ae1bf77f59a7bae8ff7ca4a51950f28
Jerome Cheng <jcheng31@gmail.com>
2013-08-25 19:07:43 +0800

    Add URL back to Models.SearchResults.

--- js/app/models/searchResults.js
+++ js/app/models/searchResults.js
@@ -1,6 +1,7 @@
 define(['jquery', 'underscore', 'backbone', 'model_search_item'], function($, _, Backbone, Models) {
 	Models.SearchResults = Backbone.Collection.extend({
 		model: Models.SearchItem,
+		url: '/service/products/search/',
 		initialize: function() {
 			this.fetch();
 		},
