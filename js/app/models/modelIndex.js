6f758e895c982819cc9bcb5e7bcb4a0cd0d8debc
Fazli Sapuan <fazli@sapuan.org>
2013-08-25 09:37:03 +0800

    debug models

--- js/app/models/modelIndex.js
+++ js/app/models/modelIndex.js
@@ -1,6 +1,8 @@
 define(['jquery', 'underscore', 'backbone', 'model_want', 'model_wants', 'model_search_item', 'model_search_results'], function($, _, Backbone) {
 
 	var Models = {};
+    
+    console.log(Models);
 
 	// Load all the models we have.
 	for (var i = 3; i < arguments.length; i++) {
@@ -8,6 +10,6 @@ define(['jquery', 'underscore', 'backbone', 'model_want', 'model_wants', 'model_
 	}
     
     console.log(Models);
-
+    
 	return Models;
 });
