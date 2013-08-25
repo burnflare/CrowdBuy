7292992aa277889b4076076f33bd4c179714a978
Jerome Cheng <jcheng31@gmail.com>
2013-08-25 18:26:23 +0800

    Move urlencode to Utils.

--- js/app/views/search.js
+++ js/app/views/search.js
@@ -4,39 +4,6 @@ define(['jquery', 'underscore', 'backbone',
 	'text!./app/views/templates/search-listings.html',
 	'models', 'utils'], function($, _, Backbone, emptySearchTemplate, searchListingTemplate, searchContainerTemplate, Models, Utils) {
 	var Views = {};
-    
-    function urlencode (str) {
-      // http://kevin.vanzonneveld.net
-      // +   original by: Philip Peterson
-      // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-      // +      input by: AJ
-      // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-      // +   improved by: Brett Zamir (http://brett-zamir.me)
-      // +   bugfixed by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-      // +      input by: travc
-      // +      input by: Brett Zamir (http://brett-zamir.me)
-      // +   bugfixed by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-      // +   improved by: Lars Fischer
-      // +      input by: Ratheous
-      // +      reimplemented by: Brett Zamir (http://brett-zamir.me)
-      // +   bugfixed by: Joris
-      // +      reimplemented by: Brett Zamir (http://brett-zamir.me)
-      // %          note 1: This reflects PHP 5.3/6.0+ behavior
-      // %        note 2: Please be aware that this function expects to encode into UTF-8 encoded strings, as found on
-      // %        note 2: pages served as UTF-8
-      // *     example 1: urlencode('Kevin van Zonneveld!');
-      // *     returns 1: 'Kevin+van+Zonneveld%21'
-      // *     example 2: urlencode('http://kevin.vanzonneveld.net/');
-      // *     returns 2: 'http%3A%2F%2Fkevin.vanzonneveld.net%2F'
-      // *     example 3: urlencode('http://www.google.nl/search?q=php.js&ie=utf-8&oe=utf-8&aq=t&rls=com.ubuntu:en-US:unofficial&client=firefox-a');
-      // *     returns 3: 'http%3A%2F%2Fwww.google.nl%2Fsearch%3Fq%3Dphp.js%26ie%3Dutf-8%26oe%3Dutf-8%26aq%3Dt%26rls%3Dcom.ubuntu%3Aen-US%3Aunofficial%26client%3Dfirefox-a'
-      str = (str + '').toString();
-
-      // Tilde should be allowed unescaped in future versions of PHP (as reflected below), but if you want to reflect current
-      // PHP behavior, you would need to add ".replace(/~/g, '%7E');" to the following.
-      return encodeURIComponent(str).replace(/!/g, '%21').replace(/'/g, '%27').replace(/\(/g, '%28').
-      replace(/\)/g, '%29').replace(/\*/g, '%2A').replace(/%20/g, '+');
-    }
 
 	Views.SearchResult = Backbone.View.extend({
 		template: _.template(searchListingTemplate),
@@ -121,7 +88,7 @@ define(['jquery', 'underscore', 'backbone',
 	Views.SearchSession = Backbone.View.extend({
 		initialize: function() {
 			var resultCollection = new Models.SearchResults({
-				url: '/service/products/search/' + urlencode(urlencode(this.searchTerm))
+				url: '/service/products/search/' + Utils.urlencode(Utils.urlencode(this.searchTerm))
 			});
 			this.searchResultView = new Views.SearchResultListing({
 				collection: resultCollection
