05d7cc1ea690aca7d19a7cf5f9b511eb212f8c20
Jerome Cheng <jcheng31@gmail.com>
2013-08-25 16:54:59 +0800

    Debug statement, don't double-render searchListingView.

--- js/app/views/search.js
+++ js/app/views/search.js
@@ -131,13 +131,13 @@ define(['jquery', 'underscore', 'backbone',
         },
 
 		searchClick: function() {
+			console.log("Clicked.");
 			Utils.loadView(this.searchListingView);
 			$('#search-results').show();
 			$('#lbl-search').text(search);
 			var search = $('#txt-search').val();
 			this.searchCollection.url = '/service/products/search/' + urlencode(urlencode(search));
 			this.searchCollection.fetch();
-			this.searchListingView.render();
 		},
 
 		initialize: function() {
