8c08dbce3fe6a468bc910c6f56b0e6f9c4c0972a
Jerome Cheng <jcheng31@gmail.com>
2013-08-25 18:37:04 +0800

    Fix incorrect reference.

--- js/app/views/search.js
+++ js/app/views/search.js
@@ -88,7 +88,7 @@ define(['jquery', 'underscore', 'backbone',
 	Views.SearchSession = Backbone.View.extend({
 		initialize: function() {
 			var resultCollection = new Models.SearchResults({
-				url: '/service/products/search/' + Utils.urlencode(Utils.urlencode(this.searchTerm))
+				url: '/service/products/search/' + Utils.urlencode(Utils.urlencode(this.options.searchTerm))
 			});
 			this.searchResultView = new Views.SearchResultListing({
 				collection: resultCollection
@@ -98,7 +98,7 @@ define(['jquery', 'underscore', 'backbone',
 
 		render: function() {
 			this.$el.html(_.template(searchContainerTemplate, { 
-				searchTerm: this.searchTerm
+				searchTerm: this.options.searchTerm
 			}));
 
 			this.searchResultView.setElement('#search-listing');
