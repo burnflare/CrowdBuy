7eab758022a017dcb5bb1a7db627bbc76672c70f
Jerome Cheng <jcheng31@gmail.com>
2013-08-26 23:12:21 +0800

    Add product details.

--- js/app/models/want.js
+++ js/app/models/want.js
@@ -17,13 +17,16 @@ define(['jquery', 'underscore', 'backbone'], function($, _, Backbone) {
 		},
 		parse: function(response) {
 			var listing = response.ProductListing;
+			var product = listing.product;
 			var attributes = {
 				id: listing.id,
 				owner: listing.creator_id,
 				dateStart: listing.date_start,
 				dateExpire: listing.date_expire,
 				location: listing.location,
-				productId: listing.product_id
+				productId: listing.product_id,
+				price: product.price,
+				imageUrl: product.images[0]
 			};
 			return attributes;
 		}
