2b1729c73fc56e52d8fcab310ee266a146b102ca
Jerome Cheng <jcheng31@gmail.com>
2013-08-31 18:39:29 +0800

    Add buyers array to want.

--- js/app/models/want.js
+++ js/app/models/want.js
@@ -18,6 +18,7 @@ define(['jquery', 'underscore', 'backbone'], function($, _, Backbone) {
 		parse: function(response) {
 			var listing = response.ProductListing;
 			var product = listing.product;
+			var buyerArray = response.Buyer;
 			var attributes = {
 				id: listing.id,
 				name: product.name,
@@ -27,7 +28,8 @@ define(['jquery', 'underscore', 'backbone'], function($, _, Backbone) {
 				location: listing.location,
 				productId: listing.product_id,
 				price: product.price,
-				imageUrl: product.images[0]
+				imageUrl: product.images[0],
+				buyers: buyerArray
 			};
 			return attributes;
 		}
