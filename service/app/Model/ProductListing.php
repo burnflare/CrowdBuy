8132ca16b072ef4b1dfcad330202caa05d19c307
Joel Low <joel@joelsplace.sg>
2013-09-01 17:00:54 +0800

    Implement isFriendsOnly to check for friends-only listings.

--- service/app/Model/ProductListing.php
+++ service/app/Model/ProductListing.php
@@ -23,9 +23,28 @@ class ProductListing extends AppModel
 		)
 	);
 	
+	/**
+	 * Checks if the given listing is owned by the person specified.
+	 * 
+	 * @param int $listingId
+	 * @param string  $personId
+	 * @return boolean
+	 */
 	public function isOwnedBy($listingId, $personId)
 	{
 		$listing = $this->findById($listingId);
 		return (string)$listing['ProductListing']['creator_id'] === (string)$personId;
+	}
+	
+	/**
+	 * Checks if a listing is only visible by friends.
+	 * 
+	 * @param int $listingId
+	 * @return boolean
+	 */
+	public function isFriendsOnly($listingId)
+	{
+		$listing = $this->findById($listingId);
+		return intval($listing['ProductListing']['friends_only']) !== 0;
 	}
 }
