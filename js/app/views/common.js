ebb01f8ca8880e594b065a645ce38b98b95cc019
Joel Low <joel@joelsplace.sg>
2013-09-05 11:49:11 +0800

    Factor out the posting to user's timeline link.

--- js/app/views/common.js
+++ js/app/views/common.js
@@ -117,14 +117,7 @@ define(['jquery', 'underscore', 'backbone',
 					product_listing_id: this.model.attributes.id
 				}
 			}).success(function() {
-				FB.api('me/crowdbuyfb:want_to_purchase', 'post',
-					{
-					  item: 'http://fb.sapuan.org/service/products/og/' + that.model.attributes.productId
-					},
-					function(response) {
-					  // handle the response
-					}
-				  );
+				Utils.postUserTimeline(that.model.attributes.productId);
 			});
 			this.model.fetch();
 		},
