cf10ddf96e4549a29385208914b29194bd47cb2b
Jerome Cheng <jcheng31@gmail.com>
2013-09-05 13:45:42 +0800

    Add comparator to sort listings in reverse chronological order.

--- js/app/models/wants.js
+++ js/app/models/wants.js
@@ -5,7 +5,7 @@ define(['jquery', 'underscore', 'backbone', 'model_want', 'utils'], function($, 
 			this.fetch({
 				dataType: 'json',
 				success: function(model, response, options) {
-					
+
 				},
 				error: function(model, response, options) {
 					if (response.status === 403) {
@@ -16,6 +16,23 @@ define(['jquery', 'underscore', 'backbone', 'model_want', 'utils'], function($, 
 		},
 		parse: function(response) {
 			return response.listings;
+		},
+
+		comparator: function(first, second) {
+			var dateValue1 = first.get("date_start");
+			var dateValue2 = second.get("date_start");
+
+			var firstDate = Date.parse(dateValue1);
+			var secondDate = Date.parse(dateValue2);
+
+			var difference = secondDate - firstDate;
+			if (difference > 0) {
+				return 1;
+			} else if (difference == 0) {
+				return 0;
+			} else {
+				return -1;
+			}
 		}
 	});
 	return Models;
