832e4d272bf6f62b1e936667cbe3a9d3aabe8227
Joel Low <joel@joelsplace.sg>
2013-09-05 08:57:59 +0800

    THe jQuery UI library is faster using Google's CDN and not the jQuery CDN.

--- js/app.js
+++ js/app.js
@@ -20,7 +20,7 @@ requirejs.config({
 	},
 	"paths": {
 		"jquery": "//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min",
-		"jquery-ui": "//code.jquery.com/ui/1.10.3/jquery-ui",
+		"jquery-ui": "//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min",
 		"facebook": "//connect.facebook.net/en_US/all",
 		"underscore": "//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.5.1/underscore-min",
 		"backbone": "//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.0.0/backbone-min",
