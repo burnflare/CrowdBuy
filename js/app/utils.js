316f921d3ed2676c01e335f743736a58d85d7846
Joel Low <joel@joelsplace.sg>
2013-09-06 17:04:02 +0800

    Take 3. This time, we specify our caption to be "CrowdBuy" instead of "apps.facebook.com"

--- js/app/utils.js
+++ js/app/utils.js
@@ -127,6 +127,7 @@ define(["jquery", "underscore", "backbone"], function($, _, Backbone) {
 					link: 'http://apps.facebook.com/509825915758193/listings/' + listingId,
 					picture: response.listing.ProductListing.product.images ?
 						response.listing.ProductListing.product.images[0] : null,
+					caption: 'CrowdBuy',
 					description: response.listing.ProductListing.product.description
 				},
 			function(response) {
