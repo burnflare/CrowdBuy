38219d7606802f3f68133b0b27cbcb8e3c952823
Vishnu Prem <vishnu@vishnuprem.com>
2013-09-06 11:45:18 +0800

    Added a Privacy Policy

--- js/app.js
+++ js/app.js
@@ -116,6 +116,7 @@ requirejs(["jquery", "underscore", "backbone", "views", "utils", 'routes', 'boot
 			var that = this;
 			$('#recommend-to-friends').click(function() { that.recommendToFriends(); })
 			$('#add-to-timeline').click(function() { that.addToTimeline(); });
+			$('#privacy-policy').click(function() { $('#privacy-policy-modal').modal('show'); });
             
             Backbone.history.start();
 		},
